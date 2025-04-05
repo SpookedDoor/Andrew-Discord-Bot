91d9461760f2ced5dbd743fed5a3f4e7dd5ba2b2
TheDragonary <112120245+TheDragonary@users.noreply.github.com>
2025-04-05 17:45:11 +0100

    Added hello.js

---
+++ andrew-bot/commands/utility/hello.js
@@ -0,0 +1,10 @@
+const { SlashCommandBuilder } = require('discord.js');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('hello')
+		.setDescription('Say hiii Androo!'),
+	async execute(interaction) {
+		await interaction.reply('hiii' + interaction.user.username + 'friend');
+	},
+};
