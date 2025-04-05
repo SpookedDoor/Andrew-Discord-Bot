2145d593c2e9064d0cde017756d20ea3345b7ec5
TheDragonary <112120245+TheDragonary@users.noreply.github.com>
2025-04-05 21:27:40 +0100

    Added griffith.js

---
+++ commands/utility/griffith.js
@@ -0,0 +1,21 @@
+const { SlashCommandBuilder } = require('discord.js');
+var today = new Date();
+var time = today.getHours();
+var message;
+
+if (time < 12) {
+	message = 'morning';
+} else if (time < 18) {
+	message = 'hello';
+} else {
+	message = 'GN';
+}
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('griffith')
+		.setDescription('Androo think he Griffith'),
+	async execute(interaction) {
+		await interaction.reply(message +' all i am griffith');
+	},
+};
