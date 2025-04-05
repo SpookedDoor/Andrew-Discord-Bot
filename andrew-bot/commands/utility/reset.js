4ca70032fa22806614b50a02f796ad631e8708c3
SpookedDoor <Charatherealchara3@gmail.com>
2025-04-05 12:24:16 -0400

    added in the files itself

---
+++ andrew-bot/commands/utility/reset.js
@@ -0,0 +1,10 @@
+const { SlashCommandBuilder } = require('discord.js');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('reset')
+		.setDescription('Resets lil Androo!'),
+	async execute(interaction) {
+		await interaction.reply('<:tomoko_cup:1358095740299116614>');
+	},
+};
