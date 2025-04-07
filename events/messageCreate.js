af508adace0c2b3f3cbeb02fc87d2169c3f93fd0
TheDragonary <112120245+TheDragonary@users.noreply.github.com>
2025-04-07 16:16:33 +0100

    Updated messageCreate.js allowing for random responses, updated reset.js to only send a tomoko cup

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -8,6 +8,17 @@ const gods = [
 	{ user: 'nonamebadass', display: 'poncho' }
 ];
 
+kanye_messages = ["Kanye the goat", "I love Kanye", "Kanye will drop new album", "new kanye interview", "Like new Kanye album?", 
+	"This aint cheddar this quiche", "I hang whit the vultures", "You like vultures also?", "I like vultures", "Vultures 2 is goated", 
+	"I got all to hang whit the vultures", "https://tenor.com/view/kanye-west-vultures-everybody-new-gif-12847039774498163445", 
+	"https://tenor.com/view/ye-kanye-kanye-vultures-vultures-listening-party-vultures-lp-gif-14111380029791063141", 
+	"https://tenor.com/view/kanye-west-gif-1846075065280866456", "https://tenor.com/view/kanye-west-kanye-ye-um-uhm-gif-1371611536126645899", 
+	"https://tenor.com/view/kanye-west-my-reaction-to-that-information-my-honest-reaction-meme-gif-15000744814966995138"]
+
+reagan_messages = ["Reagan was the best", "Reagan number 1", "Reagan number 1 president ever", "Reagan turn down the wall", "Trump and Reagan goats", 
+	"Reagan and Trump best presidents ever", "Ronald pls", "https://tenor.com/view/republican-gif-24490147", 
+	"https://tenor.com/view/ronald-reagan-reagan-republican-usa-president-gif-14605911613553531779"]
+
 module.exports = {
 	name: Events.MessageCreate,
 	execute(message) {
@@ -23,13 +34,13 @@ module.exports = {
     	const responses = [
         	{ keyword: 'hello', response: `hello ${god ? god.display : message.author.displayName} ${title}` },
         	{ keyword: 'bye', response: 'GN all i am Griffith' },
-        	{ keyword: 'cheese', response: 'This aint cheddar this quiche', response2: '<:tomoko_cup:1358095740299116614>' },
+        	{ keyword: 'cheese', response: 'https://tenor.com/view/ye-kanye-kanye-vultures-vultures-listening-party-vultures-lp-gif-14111380029791063141', response2: 'This aint cheddar this quiche' },
 			{ keyword: 'venezuela', response: 'I am from alabama' },
-			{ keyword: 'fish27.reset()', response: '<:tomoko_cup:1358095740299116614>' },
-			{ keyword: 'kanye', response: 'https://tenor.com/view/kanye-west-vultures-everybody-new-gif-12847039774498163445' },
+			{ keyword: 'fish27.reset()', response: 'hello friends', response2: '<:tomoko_cup:1358095740299116614>' },
+			{ keyword: 'kanye', response: kanye_messages[Math.floor(Math.random() * reagan_messages.length)] },
 			{ keyword: 'vultures', response: 'I got no rapper friends i hang whit the vultures' },
 		    { keyword: 'griffith', response: 'I am prime Griffith'},
-			{ keyword: 'reagan', response: 'https://tenor.com/view/republican-gif-24490147' },
+			{ keyword: 'reagan', response: reagan_messages[Math.floor(Math.random() * reagan_messages.length)] },
 			{ keyword: 'nick fuentes', response: 'nick fuentes is straight'},
 			{ keyword: 'ksi', response: 'https://tenor.com/view/from-the-screen-to-the-ring-the-the-pen-to-the-king-ksi-gif-12257927774644906851'},
     	];	
