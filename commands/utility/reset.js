9cbae70786eb9cd7fc42c5d7759730b47ee9fcb5
Dragonary <ibrahimzaman2004@gmail.com>
2025-04-07 03:05:32 +0100

    Updated reset.js with an extra tomoko cup

--- commands/utility/reset.js
+++ commands/utility/reset.js
@@ -7,16 +7,17 @@ module.exports = {
 	async execute(interaction) {
 		await interaction.reply('https://tenor.com/view/ye-kanye-kanye-vultures-vultures-listening-party-vultures-lp-gif-14111380029791063141');
 
-try {
-if (interaction.guild) {
-await interaction.channel.send('This aint cheddar this quiche');
-}
-else {
-await interaction.user.send('This aint cheddar this quiche');
-}
-}
-catch (error) {
-console.error('Follow-up failed:', error);
-}
+		try {
+			if (interaction.guild) {
+				await interaction.channel.send('This aint cheddar this quiche');
+				await interaction.channel.send('<:tomoko_cup:1358095740299116614>');
+			}
+			else {
+				await interaction.user.send('This aint cheddar this quiche');
+				await interaction.user.send('<:tomoko_cup:1358095740299116614>');
+			}
+		} catch (error) {
+			console.error('Follow-up failed:', error);
+		}
 	},
 }; 
