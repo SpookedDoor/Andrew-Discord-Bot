6e7573486f64d8a564d937030e9526d57e2def17
TheDragonary <112120245+TheDragonary@users.noreply.github.com>
2025-04-07 17:27:46 +0100

    Updated ksi.js

--- commands/utility/ksi.js
+++ commands/utility/ksi.js
@@ -1,8 +1,8 @@
 const { SlashCommandBuilder } = require('discord.js');
 
 const ksi_messages = [
-	"ksi is based", "ksi is funny", "from the screen to the ring to the pen to the king", "https://tenor.com/view/from-the-screen-to-the-ring-the-the-pen-to-the-king-ksi-gif-12257927774644906851",
-	"https://youtu.be/At8v_Yc044Y"
+	"ksi is based", "ksi is funny", "from the screen to the ring to the pen to the king", "https://youtu.be/At8v_Yc044Y",
+	"https://tenor.com/view/from-the-screen-to-the-ring-the-the-pen-to-the-king-ksi-gif-12257927774644906851"
 ];
 
 module.exports = {
