c529e1acdee353e2bd52fd02b0fe0cde354c0533
TheDragonary <112120245+TheDragonary@users.noreply.github.com>
2025-04-07 02:30:23 +0100

    Update reset.js

--- commands/utility/reset.js
+++ commands/utility/reset.js
@@ -8,9 +8,10 @@ module.exports = {
 		await interaction.reply('https://tenor.com/view/ye-kanye-kanye-vultures-vultures-listening-party-vultures-lp-gif-14111380029791063141');
 
 try {
-if (interaction.guild) {
-		await interaction.channel.send('This aint cheddar this quiche');
-}
+await interaction.followUp({
+  content: 'This aint cheddar this quiche',
+  allowedMentions: { repliedUser: false },
+});
 else {
 await interaction.user.send('This aint cheddar this quiche');
 }
