f17f4fe1669283b35ea33c9b6a6e355781a1518b
TheDragonary <112120245+TheDragonary@users.noreply.github.com>
2025-04-07 01:40:40 +0100

    Update kanye.js

--- commands/utility/kanye.js
+++ commands/utility/kanye.js
@@ -1,6 +1,6 @@
 const { SlashCommandBuilder } = require('discord.js');
 
-messages = ["Kanye the goat", "I love Kanye", "Kanye will drop new album", "new kanye interview", "Like new Kanye album?", "This aint cheddar this quiche", "I hang whit the vultures", "You like vultures also?", "I like vultures", "Vultures 2 is goated", "I got all to hang whit the vultures", 
+kanye_messages = ["Kanye the goat", "I love Kanye", "Kanye will drop new album", "new kanye interview", "Like new Kanye album?", "This aint cheddar this quiche", "I hang whit the vultures", "You like vultures also?", "I like vultures", "Vultures 2 is goated", "I got all to hang whit the vultures", 
 	"https://tenor.com/view/kanye-west-vultures-everybody-new-gif-12847039774498163445", "https://tenor.com/view/ye-kanye-kanye-vultures-vultures-listening-party-vultures-lp-gif-14111380029791063141", "https://tenor.com/view/kanye-west-gif-1846075065280866456", "https://tenor.com/view/kanye-west-kanye-ye-um-uhm-gif-1371611536126645899", "https://tenor.com/view/kanye-west-my-reaction-to-that-information-my-honest-reaction-meme-gif-15000744814966995138"]
 
 module.exports = {
@@ -8,6 +8,6 @@ module.exports = {
 		.setName('kanye')
 		.setDescription('Glaze Kanye!'),
 	async execute(interaction) {
-		await interaction.reply(messages[Math.floor(Math.random() * messages.length)]);
+		await interaction.reply(kanye_messages[Math.floor(Math.random() * kanye_messages.length)]);
 	},
 };
