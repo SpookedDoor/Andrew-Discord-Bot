536fd5161bb02c92678933213d9024b493dc667b
TheDragonary <112120245+TheDragonary@users.noreply.github.com>
2025-04-07 16:16:33 +0100

    Updated messageCreate.js allowing for random responses, updated reset.js to only send a tomoko cup

--- commands/utility/reagan.js
+++ commands/utility/reagan.js
@@ -1,6 +1,8 @@
 const { SlashCommandBuilder } = require('discord.js');
 
-reagan_messages = ["Reagan was the best", "Reagan number 1", "Reagan number 1 president ever", "Reagan turn down the wall", "Trump and Reagan goats", "Reagan and Trump best presidents ever", "Ronald pls", "https://tenor.com/view/republican-gif-24490147", "https://tenor.com/view/ronald-reagan-reagan-republican-usa-president-gif-14605911613553531779"]
+reagan_messages = ["Reagan was the best", "Reagan number 1", "Reagan number 1 president ever", "Reagan turn down the wall", "Trump and Reagan goats", 
+	"Reagan and Trump best presidents ever", "Ronald pls", "https://tenor.com/view/republican-gif-24490147", 
+	"https://tenor.com/view/ronald-reagan-reagan-republican-usa-president-gif-14605911613553531779"]
 
 module.exports = {
 	data: new SlashCommandBuilder()
--- commands/utility/reset.js
+++ commands/utility/reset.js
@@ -5,19 +5,6 @@ module.exports = {
 		.setName('reset')
 		.setDescription('Resets lil Androo!'),
 	async execute(interaction) {
-		await interaction.reply('https://tenor.com/view/ye-kanye-kanye-vultures-vultures-listening-party-vultures-lp-gif-14111380029791063141');
-
-		try {
-			if (interaction.guild) {
-				await interaction.channel.send('This aint cheddar this quiche');
-				await interaction.channel.send('<:tomoko_cup:1358095740299116614>');
-			}
-			else {
-				await interaction.user.send('This aint cheddar this quiche');
-				await interaction.user.send('<:tomoko_cup:1358095740299116614>');
-			}
-		} catch (error) {
-			console.error('Follow-up failed:', error);
-		}
+		await interaction.reply('<:tomoko_cup:1358095740299116614>');
 	},
 }; 
