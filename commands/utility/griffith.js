7366f851067c514a772391f75f7d5a3fa60430bb
TheDragonary <112120245+TheDragonary@users.noreply.github.com>
2025-04-07 16:51:48 +0100

    Updated messageCreate.js and griffith.js

--- commands/utility/griffith.js
+++ commands/utility/griffith.js
@@ -1,23 +1,13 @@
 const { SlashCommandBuilder } = require('discord.js');
-var today = new Date();
-var time = today.getHours();
-var message;
 
-if (time < 12) {
-	message = 'morning';
-} else if (time < 18) {
-	message = 'hello';
-} else {
-	message = 'GN';
-}
-
-const griffith_messages = [message + ' all i am Griffith', 'I am prime Griffith', 'https://i.imgflip.com/9q0wk1.jpg'];
+let timed_message = new Date().getHours() < 12 ? 'morning' : new Date().getHours() < 18 ? 'hello' : 'GN';
+const griffith_messages = [timed_message + ' all i am Griffith', 'I am prime Griffith', 'https://i.imgflip.com/9q0wk1.jpg'];
 
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('griffith')
 		.setDescription('Androo think he Griffith'),
 	async execute(interaction) {
-		await interaction.reply(Math.floor(Math.random() * griffith_messages.length));
+		await interaction.reply(griffith_messages[Math.floor(Math.random() * griffith_messages.length)]);
 	},
 };
