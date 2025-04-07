4340d9b41a5a5e351f1c1ed69bff213ddeb1457b
TheDragonary <112120245+TheDragonary@users.noreply.github.com>
2025-04-07 13:29:36 +0100

    Removed unused variables, removed comments, removed ability to delete guild-specific commands from delete-commands.js (all commands are global so that's useless)

--- delete-commands.js
+++ delete-commands.js
@@ -1,16 +1,9 @@
 const { REST, Routes } = require('discord.js');
-const { clientId, guildId, token } = require('./config.json');
+const { clientId, token } = require('./config.json');
 
 const rest = new REST().setToken(token);
 
-// ...
-
-// for guild-based commands
-rest.delete(Routes.applicationGuildCommand(clientId, guildId, '1358099624509833297'))
-	.then(() => console.log('Successfully deleted guild command'))
-	.catch(console.error);
-
-// for global commands. If it ain't obvious, the numbers inserted here is the commandId. Replace that with whatever command you wanna delete :iminnocent: Same with the guild-based one.
+// for global commands. If it ain't obvious, the numbers inserted here is the commandId. Replace that with whatever command you wanna delete :iminnocent:
 rest.delete(Routes.applicationCommand(clientId, '1358099624509833297'))
 	.then(() => console.log('Successfully deleted application command'))
 	.catch(console.error);
