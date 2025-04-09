c0cea2c093227af3d1ee3d00d3ace539a1463b17
TheDragonary <112120245+TheDragonary@users.noreply.github.com>
2025-04-09 22:05:03 +0100

    Added image.js and griffith.png, and changed the griffith image to an attachment instead of link

---
+++ commands/utility/image.js
@@ -0,0 +1,43 @@
+const { SlashCommandBuilder, MessageFlags, PermissionsBitField } = require('discord.js');
+
+const gods = [
+	{ user: 'thedragonary', display: 'dragonary' },
+	{ user: 'spookeddoor', display: 'spookeddoor' },
+];
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('image')
+		.setDescription('ADMIN: Make Androo send an image!')
+		.addAttachmentOption(option =>
+			option.setName('image')
+				.setDescription('Image')
+				.setRequired(true)),
+	async execute(interaction) {
+		try {
+			if (
+				gods.find(g => interaction.user.username === g.user) ||
+				interaction.member?.permissions?.has(PermissionsBitField.Flags.ManageGuild) ||
+				!interaction.guild
+			) {
+				const attachment = interaction.options.getAttachment('image').url;
+				await interaction.reply({ content: `Image sent: ${attachment}`, flags: MessageFlags.Ephemeral });
+				if (interaction.guild) {
+					await interaction.channel.send({ files: [attachment] });
+					return;
+				}
+				else {
+					await interaction.followUp({ files: [attachment] });
+					return;
+				}
+			}
+			else {
+                await interaction.reply({ content: "You are not authorised to use this command.", flags: MessageFlags.Ephemeral });
+				return;
+			}
+		} catch (error) {
+			console.error(error);
+			await interaction.reply({ content: "An error occurred while executing this command.", flags: MessageFlags.Ephemeral });
+		}
+	},
+};
