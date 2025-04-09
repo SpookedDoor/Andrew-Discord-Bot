3102588b930795f2e2bf2c70f72d7e240505104e
SpookedDoor <Charatherealchara3@gmail.com>
2025-04-09 13:43:42 -0400

    Added yet another possibleMessages list.

--- events/ready.js
+++ events/ready.js
@@ -1,7 +1,25 @@
 const { Events } = require("discord.js");
 const { channelMap } = require("../config.json");
 const status = require('../setSleep.js');
-const { possibleMessages, possibleMessages2, possibleMessages3 } = require('../messageDatabase.js');
+const { possibleMessages, possibleMessages2, possibleMessages3, possibleMessages4 } = require('../messageDatabase.js');
+
+function checkSleepSchedule() {
+	const now = new Date();
+	const hourUTC = now.getUTCHours();
+
+	if (hourUTC >= 2 && hourUTC < 12) {
+		if (!status.isAsleep) {
+			console.log('Auto-sleeping Androo');
+			status.setAsleep(true);
+		}
+	}
+	else {
+		if (status.isAsleep && !status.override	) {
+			console.log('Auto-waking Androo');
+			status.setAsleep(false);
+		}
+	}
+}
 
 module.exports = {
     name: Events.ClientReady,
@@ -9,7 +27,7 @@ module.exports = {
     execute(client) {
         console.log(`Ready! Logged in as ${client.user.tag}`);
 
-        const allMessages = possibleMessages.concat(possibleMessages2, possibleMessages3);
+        const allMessages = possibleMessages.concat(possibleMessages2, possibleMessages3, possibleMessages4);
 
         const sendRandomMessage = async () => {
             for (const [guildId, channelId] of Object.entries(channelMap)) {
@@ -21,7 +39,7 @@ module.exports = {
                 }
     
 				try {
-					if (!status.getSleepStatus(guildId)) {
+					if (!status.isAsleep) {
 						const randomMessage = allMessages[Math.floor(Math.random() * allMessages.length)];
 						
 						if (possibleMessages2.includes(randomMessage)) {
@@ -33,11 +51,20 @@ module.exports = {
 							await channel.send(possibleMessages3[0]);
 							await channel.send(possibleMessages3[1]);
 							console.log(`Both messages from possibleMessages3 sent to guild ID: ${guildId}`);
+
+            }
+              else if (possibleMessages4.includes(randomMessage)) {
+              await channel.send(possibleMessages4[0]);
+              await channel.send(possibleMessages4[1]);
+              console.log(`Both messages from possibleMessages4 sent to guild ID: ${guildId}`);
 						}
 						else {
 							await channel.send(randomMessage);
 							console.log(`Random message sent to guild ID: ${guildId}`);
-						}
+            }
+          }
+          else {
+              console.error('Random message is undefined or null!');
 					}
 				} catch (error) {
 					console.error(`Error sending message to guild ID: ${guildId}`, error);
@@ -47,32 +74,12 @@ module.exports = {
         };
 
         const scheduleRandomMessage = () => {
-			if (!status.getSleepStatus(client.guilds.cache.first().id)) {
+			if (!status.isAsleep) {
 				const randomDelay = Math.floor(Math.random() * (3600000 - 1800000 + 1)) + 1800000;
 				console.log(`Next message will be sent in ${Math.round(randomDelay / 60000)} minutes.`);
 				setTimeout(sendRandomMessage, randomDelay);
 			}
         };
-
-		const checkSleepSchedule = () => {
-			const now = new Date();
-			const hourUTC = now.getUTCHours();
-			
-			for (const [guildId] of Object.entries(channelMap)) {
-				if (hourUTC >= 2 && hourUTC < 12) {
-					if (!status.getSleepStatus(guildId)) {
-						console.log(`Auto-sleeping Androo in ${client.guilds.cache.get(guildId).name}`);
-						status.setSleepStatus(guildId, true);	
-					}
-				}
-				else {
-					if (status.getSleepStatus(guildId) && !status.getOverride(guildId)	) {
-						console.log(`Auto-waking Androo in ${client.guilds.cache.get(guildId).name}`);
-						status.setSleepStatus(guildId, false);
-					}
-				}
-			}
-		}
 
 		scheduleRandomMessage();
 
