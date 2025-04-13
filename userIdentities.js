29777d5c09df1c263a35bc7b452354a6394ee0db
TheDragonary <112120245+TheDragonary@users.noreply.github.com>
2025-04-13 15:55:04 +0100

    Updated users

--- userIdentities.js
+++ userIdentities.js
@@ -1,63 +1,91 @@
 const users = [
     {
-        id: '123456789012345678',
+        id: '1181721653634420767',
         usernames: ['thedragonary', 'dragonary'],
         displayName: 'Dragonary',
-        isCreator: true
+        isCreator: true,
+        isGod: true,
+        traits: ['male', 'creator of andrew bot', 'british'],
     },
     {
-        id: '234567890123456789',
+        id: '956743571980038174',
         usernames: ['spookeddoor', 'spooked'],
         displayName: 'SpookedDoor',
-        isCreator: true
+        isCreator: true,
+        isGod: true,
+        traits: ['male', 'creator of andrew bot', 'american'],
     },
     {
-        id: '345678901234567890',
+        id: '1208629217890148363',
         usernames: ['hellbeyv2', 'hellbey'],
         displayName: 'Hellbey',
-        isCreator: false
+        isCreator: false,
+        isGod: true,
+        traits: ['male', 'american'],
     },
     {
-        id: '456789012345678901',
+        id: '545586677117353985',
         usernames: ['sillyh.', 'trinke'],
         displayName: 'Trinke',
-        isCreator: false
+        isCreator: false,
+        isGod: true,
+        traits: ['male', 'lithuanian'],
     },
     {
-        id: '567890123456789012',
+        id: '197487122041667584',
         usernames: ['nonamebadass', 'poncho'],
         displayName: 'Poncho',
-        isCreator: false
+        isCreator: false,
+        isGod: true,
+        traits: ['male', 'degenerate', 'american'],
     },
     {
-        id: '678901234567890123',
+        id: '1047876190809116752',
         usernames: ['moonmanv2', 'moon man', 'moonman', 'moonie'],
         displayName: 'Moon Man',
-        isCreator: false
+        isCreator: false,
+        isGod: false,
+        traits: ['male', 'brazilian'],
     },
     {
-        id: '789012345678901234',
+        id: '559520799829000203',
         usernames: ['marv_mari', 'brit'],
         displayName: 'Brit',
-        isCreator: false
+        isCreator: false,
+        isGod: false,
+        traits: ['female', 'american', 'russian', 'futanari', 'married to Tomoko'],
     },
     {
-        id: '890123456789012345',
+        id: '689829347443605768',
         usernames: ['edenlance', 'peanut', 'penut'],
         displayName: 'Penut',
-        isCreator: false
+        isCreator: false,
+        isGod: false,
+        traits: ['female', 'american'],
     },
     {
-        id: '901234567890123456',
+        id: null,
         usernames: ['nagiro.', 'ghostto'],
         displayName: 'Ghostto',
-        isCreator: false
+        isCreator: false,
+        isGod: false,
+        traits: ['male', 'degenerate', 'american']
     },
     {
-        id: '012345678901234567',
+        id: '776931705813860363',
         usernames: ['meeperthe1', 'meeper'],
         displayName: 'Meeper',
-        isCreator: false
+        isCreator: false,
+        isGod: false,
+        traits: ['male', 'likes warhammer', 'american']
+    },
+    {
+        id: '1014404029146726460',
+        usernames: ['andrew143256', 'andrew', 'fish 27'],
+        displayName: 'Andrew',
+        isCreator: true,
+        isGod: false,
+        traits: ['the real andrew', 'the person who andrew bot is based on'],
     },
 ];
 
