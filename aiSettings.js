7e1bc9528cafb28da6f745eb1703daca3ad7ff6a
TheDragonary <112120245+TheDragonary@users.noreply.github.com>
2025-06-11 18:35:56 +0100

    Switched to Chutes

--- aiSettings.js
+++ aiSettings.js
@@ -1,19 +1,24 @@
 require('dotenv').config();
 
-// Change to a different URL if not using KoboldCPP, examples are shown below
-// https://openrouter.ai/api/v1
+// Change to a different URL if not using Chutes, examples are shown below
 // https://llm.chutes.ai/v1
-const baseURL = "http://localhost:5001/v1";
+// https://openrouter.ai/api/v1
+// http://localhost:5001/v1
+const baseURL = "https://llm.chutes.ai/v1";
 
-// If using an online AI service, change this to your API key stored in ".env"
-// Example: process.env.CHUTES_API_KEY
-const apiKey = "0";
+// If using a different online AI service, change this to your API key stored in ".env"
+// Examples: process.env.CHUTES_API_KEY, process.env.OPENROUTER_API_KEY
+// If using local AI, change this to "0", although it doesn't really matter and you can leave it as-is
+const apiKey = process.env.CHUTES_API_KEY;
 
-const gptModel = "koboldcpp"; // for gpt.js
-const gptimageModel = "koboldcpp"; // for gptimage.js
+// If using koboldcpp, change ALL of these to "koboldcpp"
+// For vision to work locally, download the correct mmproj from https://huggingface.co/koboldcpp/mmproj/tree/main
+// Example: If you are using a model based on Llama3, download the one that says Llama3, then you would insert it into Loaded Files > Vision mmproj
+const gptModel = "deepseek-ai/DeepSeek-V3-0324"; // for gpt.js
+const gptimageModel = "chutesai/Llama-4-Maverick-17B-128E-Instruct-FP8"; // for gptimage.js
 
 // for messageCreate.js
-const messageModel = "koboldcpp";
-const messageImageModel = "koboldcpp";
+const messageModel = "deepseek-ai/DeepSeek-V3-0324";
+const messageImageModel = "chutesai/Llama-4-Maverick-17B-128E-Instruct-FP8";
 
 module.exports = { baseURL, apiKey, gptModel, gptimageModel, messageModel, messageImageModel };
