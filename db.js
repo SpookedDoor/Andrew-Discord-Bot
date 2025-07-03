55e215f81235c2c2997094b12cda64a3aeaa5344
TheDragonary <112120245+TheDragonary@users.noreply.github.com>
2025-07-03 16:17:53 +0100

    Switch to PostgreSQL database to store Last.fm data

---
+++ db.js
@@ -0,0 +1,12 @@
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  host: process.env.PGHOST,
+  user: process.env.PGUSER,
+  password: process.env.PGPASSWORD,
+  database: process.env.PGDATABASE,
+  port: process.env.PGPORT,
+  ssl: process.env.PGSSLMODE === 'require' ? { rejectUnauthorized: false } : false,
+});
+
+module.exports = pool;
