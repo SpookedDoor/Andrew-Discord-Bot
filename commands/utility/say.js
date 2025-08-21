a1efe386a7d2ad2ed0e0a7669c86333d7ebd9d8d
TheDragonary <112120245+TheDragonary@users.noreply.github.com>
2025-08-22 00:25:41 +0100

    Combine image.js into say.js

--- commands/utility/say.js
+++ commands/utility/say.js
@@ -5,35 +5,30 @@ module.exports = {
 		.setName('say')
 		.setDescription('ADMIN: Make Androo say something!')
 		.addStringOption(option =>
-			option.setName('input')
-				.setDescription('Input message')
-				.setRequired(true)),
+			option.setName('text')
+				.setDescription('Text message')
+				.setRequired(false))
+		.addAttachmentOption(option =>
+			option.setName('image')
+				.setDescription('Image')
+				.setRequired(false)),
 	async execute(interaction) {
 		try {
 			const allowedIds = ['1181721653634420767', '956743571980038174'];
-			if (
-				allowedIds.includes(interaction.user.id) ||
-				interaction.member?.permissions?.has(PermissionsBitField.Flags.ManageGuild) ||
-				!interaction.guild
-			) {
-				const message = interaction.options.getString('input');
+			if (!(allowedIds.includes(interaction.user.id) || interaction.member?.permissions?.has(PermissionsBitField.Flags.ManageGuild) || !interaction.guild)) return await interaction.reply({ content: "You are not authorised to use this command.", flags: MessageFlags.Ephemeral });
 
-				console.log(`/say command used by: ${interaction.user.username}\nLocation: ${interaction.guild ? `${interaction.guild.name} - ${interaction.channel.name}` : `${interaction.user.username} - DM`}\nMessage: ${message}`);
+			let message = interaction.options.getString('text');
+			const attachment = interaction.options.getAttachment('image');
+			if (!message && !attachment) return await interaction.reply({ content: "Please provide a message or an image.", flags: MessageFlags.Ephemeral });
 
-				await interaction.reply({ content: `Message sent: ${message}`, flags: MessageFlags.Ephemeral });
-				if (interaction.guild) {
-					await interaction.channel.send(message);
-					return;
-				}
-				else {
-					await interaction.followUp(message);
-					return;
-				}
+			console.log(`/say command used by: ${interaction.user.username}\nLocation: ${interaction.guild ? `${interaction.guild.name} - ${interaction.channel.name}` : `${interaction.user.username} - DM`}\nMessage: ${message}`);
+			if (attachment) {
+				console.log(`Attachment: ${attachment.url}`);
+				message = { content: message, files: [attachment] };
 			}
-			else {
-                await interaction.reply({ content: "You are not authorised to use this command.", flags: MessageFlags.Ephemeral });
-				return;
-			}
+
+			await interaction.reply({ content: `Message sent!`, flags: MessageFlags.Ephemeral });
+			if (interaction.guild) return await interaction.channel.send(message); else return await interaction.followUp(message);
 		} catch (error) {
 			console.error(error);
 			await interaction.reply({ content: "An error occurred while executing this command.", flags: MessageFlags.Ephemeral });
