7d82d903365e28adf394738f05977b596f75ac95
TheDragonary <112120245+TheDragonary@users.noreply.github.com>
2025-08-22 01:20:50 +0100

    Combine say commands

--- commands/utility/say.js
+++ commands/utility/say.js
@@ -1,19 +1,47 @@
 const { SlashCommandBuilder, MessageFlags, PermissionsBitField } = require('discord.js');
+const { getAge } = require('../../messageDatabase.js');
 
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('say')
 		.setDescription('ADMIN: Make Androo say something!')
-		.addStringOption(option =>
-			option.setName('text')
-				.setDescription('Text message')
-				.setRequired(false))
-		.addAttachmentOption(option =>
-			option.setName('image')
-				.setDescription('Image')
-				.setRequired(false)),
+		.addSubcommand(subcommand =>
+        subcommand
+            .setName('message')
+            .setDescription('ADMIN: Make Androo say something or send an image!')
+            .addStringOption(option =>
+                option.setName('text')
+                    .setDescription('Text message')
+                    .setRequired(false))
+            .addAttachmentOption(option =>
+                option.setName('image')
+                    .setDescription('Image')
+                    .setRequired(false))
+		)
+		.addSubcommandGroup(group =>
+			group
+				.setName('my')
+				.setDescription('Say my name')
+				.addSubcommand(subcommand =>
+					subcommand
+						.setName('name')
+						.setDescription('Say my name')
+				)
+				.addSubcommand(subcommand =>
+					subcommand
+						.setName('age')
+						.setDescription('Say my age')
+				)
+		),
 	async execute(interaction) {
 		try {
+			if (interaction.options.getSubcommand(false) === 'name') {
+				await interaction.reply("https://tenor.com/view/waltwhite-breakingbad-say-my-name-gif-7259290");
+				return await interaction.followUp("Andrew Valenzuela");
+			}
+
+			if (interaction.options.getSubcommand(false) === 'age') return await interaction.reply(`I am ${getAge()}`);
+
 			const allowedIds = ['1181721653634420767', '956743571980038174'];
 			if (!(allowedIds.includes(interaction.user.id) || interaction.member?.permissions?.has(PermissionsBitField.Flags.ManageGuild) || !interaction.guild)) return await interaction.reply({ content: "You are not authorised to use this command.", flags: MessageFlags.Ephemeral });
 
