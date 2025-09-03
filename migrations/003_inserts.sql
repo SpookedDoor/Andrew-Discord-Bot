05ea532e89d438d18f56b1f72d01d8b7d8b1030f
TheDragonary <112120245+TheDragonary@users.noreply.github.com>
2025-09-03 18:20:16 +0100

    Create SQL files

---
+++ migrations/003_inserts.sql
@@ -0,0 +1,150 @@
+INSERT INTO messages (category_id, content) VALUES
+-- Happy fucker
+((SELECT id FROM message_categories WHERE name = 'happy_fucker'), 'https://tenor.com/view/sailor-moon-anime-amazed-gif-4474073'),
+((SELECT id FROM message_categories WHERE name = 'happy_fucker'), '<:cirnoarc:1358517895809990793>'),
+((SELECT id FROM message_categories WHERE name = 'happy_fucker'), '<:tomokoarc:1358500281956044991>'),
+((SELECT id FROM message_categories WHERE name = 'happy_fucker'), '<:pekostare:1365786858465919046>'),
+((SELECT id FROM message_categories WHERE name = 'happy_fucker'), '<:umarucry:1358518905219584120>'),
+((SELECT id FROM message_categories WHERE name = 'happy_fucker'), 'OMG'),
+((SELECT id FROM message_categories WHERE name = 'happy_fucker'), 'Based'),
+((SELECT id FROM message_categories WHERE name = 'happy_fucker'), 'Nice'),
+((SELECT id FROM message_categories WHERE name = 'happy_fucker'), 'Niceee'),
+((SELECT id FROM message_categories WHERE name = 'happy_fucker'), 'Wow'),
+((SELECT id FROM message_categories WHERE name = 'happy_fucker'), 'Pog'),
+((SELECT id FROM message_categories WHERE name = 'happy_fucker'), 'griffith bars'),
+((SELECT id FROM message_categories WHERE name = 'happy_fucker'), 'https://tenor.com/view/squirtle-pokemon-gif-23882585'),
+((SELECT id FROM message_categories WHERE name = 'happy_fucker'), 'YES'),
+((SELECT id FROM message_categories WHERE name = 'happy_fucker'), 'Yesssss'),
+((SELECT id FROM message_categories WHERE name = 'happy_fucker'), 'https://tenor.com/view/sunshine-mario-mario-kart-mario-kart-tour-summer-tours-rule-mario-gif-22350487'),
+((SELECT id FROM message_categories WHERE name = 'happy_fucker'), ':0'),
+((SELECT id FROM message_categories WHERE name = 'happy_fucker'), 'BASEEED'),
+((SELECT id FROM message_categories WHERE name = 'happy_fucker'), 'https://tenor.com/view/cirno-cirno-fumo-fumo-gif-21728275'),
+((SELECT id FROM message_categories WHERE name = 'happy_fucker'), 'https://tenor.com/view/himouto-umaru-chan-cute-anime-christmas-happy-gif-16094038'),
+((SELECT id FROM message_categories WHERE name = 'happy_fucker'), 'Basedgod'),
+((SELECT id FROM message_categories WHERE name = 'happy_fucker'), 'Griffith wold be proud of you'),
+((SELECT id FROM message_categories WHERE name = 'happy_fucker'), '<:wholesome:1403161256189493350>'),
+((SELECT id FROM message_categories WHERE name = 'happy_fucker'), 'Heaven'),
+
+-- Upset fucker
+((SELECT id FROM message_categories WHERE name = 'upset_fucker'), 'https://tenor.com/view/cry-tear-sad-max-cry-sad-max-gif-8713574713734916'),
+((SELECT id FROM message_categories WHERE name = 'upset_fucker'), 'Nonono'),
+((SELECT id FROM message_categories WHERE name = 'upset_fucker'), '<:depressed:1358517922938617883>'),
+((SELECT id FROM message_categories WHERE name = 'upset_fucker'), '<:umarucry:1358518905219584120>'),
+((SELECT id FROM message_categories WHERE name = 'upset_fucker'), 'Bruh'),
+((SELECT id FROM message_categories WHERE name = 'upset_fucker'), 'wtf'),
+((SELECT id FROM message_categories WHERE name = 'upset_fucker'), 'stfu'),
+((SELECT id FROM message_categories WHERE name = 'upset_fucker'), 'hater'),
+((SELECT id FROM message_categories WHERE name = 'upset_fucker'), 'This hater'),
+((SELECT id FROM message_categories WHERE name = 'upset_fucker'), 'Chill hater'),
+((SELECT id FROM message_categories WHERE name = 'upset_fucker'), 'The envy'),
+((SELECT id FROM message_categories WHERE name = 'upset_fucker'), 'You envy me'),
+((SELECT id FROM message_categories WHERE name = 'upset_fucker'), 'Ahahhaah you wish'),
+((SELECT id FROM message_categories WHERE name = 'upset_fucker'), 'You Envy me couse i am American'),
+((SELECT id FROM message_categories WHERE name = 'upset_fucker'), 'i am a fascist you Envy me'),
+((SELECT id FROM message_categories WHERE name = 'upset_fucker'), 'You Envy me because i am  a rightchad'),
+((SELECT id FROM message_categories WHERE name = 'upset_fucker'), 'Cope harder'),
+((SELECT id FROM message_categories WHERE name = 'upset_fucker'), 'Cope harder you all'),
+((SELECT id FROM message_categories WHERE name = 'upset_fucker'), 'Cope'),
+((SELECT id FROM message_categories WHERE name = 'upset_fucker'), 'cope all'),
+((SELECT id FROM message_categories WHERE name = 'upset_fucker'), 'cope more'),
+((SELECT id FROM message_categories WHERE name = 'upset_fucker'), 'Shut up'),
+((SELECT id FROM message_categories WHERE name = 'upset_fucker'), 'Shut up communist'),
+((SELECT id FROM message_categories WHERE name = 'upset_fucker'), 'Shut up i am white'),
+((SELECT id FROM message_categories WHERE name = 'upset_fucker'), 'https://tenor.com/view/venere-angry-mad-evil-laugh-gif-15340793'),
+((SELECT id FROM message_categories WHERE name = 'upset_fucker'), 'Why you are a leftard'),
+((SELECT id FROM message_categories WHERE name = 'upset_fucker'), 'Fake'),
+((SELECT id FROM message_categories WHERE name = 'upset_fucker'), 'Average zoomer'),
+((SELECT id FROM message_categories WHERE name = 'upset_fucker'), 'Not even clicking if Is playboi carti'),
+((SELECT id FROM message_categories WHERE name = 'upset_fucker'), 'What a horror'),
+((SELECT id FROM message_categories WHERE name = 'upset_fucker'), 'You sick of the head'),
+((SELECT id FROM message_categories WHERE name = 'upset_fucker'), 'you trolling'),
+((SELECT id FROM message_categories WHERE name = 'upset_fucker'), 'stop trolling'),
+((SELECT id FROM message_categories WHERE name = 'upset_fucker'), '-999999999 social Watamote points'),
+((SELECT id FROM message_categories WHERE name = 'upset_fucker'), 'stop whit that  trolling pls'),
+((SELECT id FROM message_categories WHERE name = 'upset_fucker'), 'Hell nah'),
+((SELECT id FROM message_categories WHERE name = 'upset_fucker'), 'This guy'),
+((SELECT id FROM message_categories WHERE name = 'upset_fucker'), 'Actually getting destroyed by a bot  rn'),
+((SELECT id FROM message_categories WHERE name = 'upset_fucker'), 'Dont click'),
+((SELECT id FROM message_categories WHERE name = 'upset_fucker'), 'Shut the fuck up liberal'),
+((SELECT id FROM message_categories WHERE name = 'upset_fucker'), 'Antifa bastard'),
+
+-- SSS Tier Opinions
+((SELECT id FROM message_categories WHERE name = 'sss_tier_opinions'), 'Sabaton albums are top tier sss+'),
+((SELECT id FROM message_categories WHERE name = 'sss_tier_opinions'), 'Reagan Top Tier SSS+ president'),
+((SELECT id FROM message_categories WHERE name = 'sss_tier_opinions'), 'Reagan SSS+ president'),
+((SELECT id FROM message_categories WHERE name = 'sss_tier_opinions'), 'Kanye new music Is good\nCousins\nFree diddy\nCosby\nTop Tier SSS+ songs\nAlso nitrous'),
+((SELECT id FROM message_categories WHERE name = 'sss_tier_opinions'), 'i like is content of nickavocado eating takis and pizza <:tomoko_cup:1358095740299116614>\nmcdonalds content is SSS+ top tier too'),
+((SELECT id FROM message_categories WHERE name = 'sss_tier_opinions'), 'Boil crab\nSSS+ alabamian cousine'),
+((SELECT id FROM message_categories WHERE name = 'sss_tier_opinions'), 'Watamote SSS+ manga\nAlso  that one of the vampire guy'),
+((SELECT id FROM message_categories WHERE name = 'sss_tier_opinions'), 'Alabama SSS+ Tier state'),
+((SELECT id FROM message_categories WHERE name = 'sss_tier_opinions'), 'alabama seafood +SSS tier'),
+((SELECT id FROM message_categories WHERE name = 'sss_tier_opinions'), 'KFC SSS+ Tier'),
+((SELECT id FROM message_categories WHERE name = 'sss_tier_opinions'), 'Freddy fazbear pizza SSSS+ Tier'),
+((SELECT id FROM message_categories WHERE name = 'sss_tier_opinions'), 'Alabamian cousine'), -- ./media/alabamachicken.jpg
+((SELECT id FROM message_categories WHERE name = 'sss_tier_opinions'), 'SSS+ burger from alabama\nAlso alabamian tomatoe'), -- ./media/alabamaburger.jpg
+
+-- Hello Followup
+((SELECT id FROM message_categories WHERE name = 'hello_followup'), 'Gonna have the red Button someday'),
+((SELECT id FROM message_categories WHERE name = 'hello_followup'), 'You like watamote?'),
+((SELECT id FROM message_categories WHERE name = 'hello_followup'), 'Thougs on nuclear weaponry?'),
+((SELECT id FROM message_categories WHERE name = 'hello_followup'), 'Gonna claim the Sea\nSomeday\nAlso the world'),
+((SELECT id FROM message_categories WHERE name = 'hello_followup'), '<:tomoko_cup:1358095740299116614>'),
+((SELECT id FROM message_categories WHERE name = 'hello_followup'), '<:cirnoarc:1358517895809990793>'),
+((SELECT id FROM message_categories WHERE name = 'hello_followup'), '<:tomokoarc:1358500281956044991>'),
+((SELECT id FROM message_categories WHERE name = 'hello_followup'), 'you like watchmen?'),
+((SELECT id FROM message_categories WHERE name = 'hello_followup'), 'Like 100gecs?'),
+((SELECT id FROM message_categories WHERE name = 'hello_followup'), 'like KFC?'),
+((SELECT id FROM message_categories WHERE name = 'hello_followup'), 'Like new Kanye song?'),
+((SELECT id FROM message_categories WHERE name = 'hello_followup'), 'how you do?'),
+((SELECT id FROM message_categories WHERE name = 'hello_followup'), 'You like vultures 2?'),
+((SELECT id FROM message_categories WHERE name = 'hello_followup'), 'Rightchads');
+
+-- Attachments
+INSERT INTO message_attachments (message_id, file_path)
+SELECT id, './media/alabamachicken.jpg'
+FROM messages
+WHERE content = 'Alabamian cousine';
+
+INSERT INTO message_attachments (message_id, file_path)
+SELECT id, './media/alabamaburger.jpg'
+FROM messages
+WHERE content = 'SSS+ burger from alabama\nAlso alabamian tomatoe';
+
+INSERT INTO message_attachments (message_id, file_path)
+SELECT m.id, v.file_path
+FROM messages m
+JOIN (
+    VALUES
+        ('./media/gangshi.png'),
+        ('./media/shadowbunny.jpg'),
+        ('./media/banner.mp4'),
+        ('./media/andrewx.jpg')
+) AS v(file_path) ON true
+WHERE m.category_id = (SELECT id FROM message_categories WHERE name = 'general') AND m.content IS NULL;
+
+INSERT INTO message_attachments (message_id, file_path)
+SELECT m.id, v.file_path
+FROM messages m
+JOIN (
+    VALUES
+        ('./media/griffith.png'),
+        ('./media/griffith2.jpg')
+) AS v(file_path) ON true
+WHERE m.category_id = (SELECT id FROM message_categories WHERE name = 'griffith') AND m.content IS NULL;
+
+INSERT INTO attachment_triggers (trigger_text, category) VALUES
+('griffith', 'griffith'),
+('femto', 'griffith'),
+('falconia', 'griffith'),
+('burger', 'burger'),
+('chicken', 'chicken'),
+('wtf', 'wtf'),
+('alien x', 'alien x');
+
+INSERT INTO attachment_files (category, file_path) VALUES
+('griffith', './media/griffith.png'),
+('griffith', './media/griffith2.jpg'),
+('burger', './media/alabamaburger.jpg'),
+('chicken', './media/alabamachicken.jpg'),
+('wtf', './media/WTF.png'),
+('alien x', './media/andrewx.jpg');
