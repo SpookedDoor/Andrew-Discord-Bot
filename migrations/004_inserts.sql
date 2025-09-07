62fbcf6b6beb0f128ca1b668ed006e7ac2475534
TheDragonary <112120245+TheDragonary@users.noreply.github.com>
2025-09-07 18:45:58 +0100

    Fix minor errors in the new inserts

--- migrations/004_inserts.sql
+++ migrations/004_inserts.sql
@@ -1,15 +1,13 @@
 -- Insert messages for each category
 INSERT INTO messages (category_id, content) VALUES
-
 -- General
 ((SELECT id FROM message_categories WHERE name = 'general'), 'I want a nazi girl someday'),
 ((SELECT id FROM message_categories WHERE name = 'general'), 'Eat crab whit ranch better'),
 ((SELECT id FROM message_categories WHERE name = 'general'), 'Seafood the best'),
-((SELECT id FROM message_categories WHERE name = 'general'), 'Freddy wold eat burgers\nChica wold eat tacos\nFoxy wold well done ribs\nGolden Freddy wold eat Pineaple Pizza\nMangle wold eat lemon cake\nToy Freddy wold eat fishcake\
-nAndrew wold eat alabamian crab boil'),
+((SELECT id FROM message_categories WHERE name = 'general'), 'Freddy wold eat burgers\nChica wold eat tacos\nFoxy wold well done ribs\nGolden Freddy wold eat Pineaple Pizza\nMangle wold eat lemon cake\nToy Freddy wold eat fishcake\nAndrew wold eat alabamian crab boil'),
 ((SELECT id FROM message_categories WHERE name = 'general'), 'i want SS role pls'),
 ((SELECT id FROM message_categories WHERE name = 'general'), 'SS Wagner role\nGrey color?'),
-((SELECT id FROM message_categories WHERE name = 'general'), "Niggas a'int gonna sleep until see watamote season two"),
+((SELECT id FROM message_categories WHERE name = 'general'), 'Niggas a''int gonna sleep until see watamote season two'),
 ((SELECT id FROM message_categories WHERE name = 'general'), 'Screamer video?'),
 ((SELECT id FROM message_categories WHERE name = 'general'), 'Real SS Wagner hours'),
 ((SELECT id FROM message_categories WHERE name = 'general'), 'https://tenor.com/view/moon-moon-song-gabixe-moon-dance-gif-15770315873171635957'),
@@ -60,11 +58,11 @@ nAndrew wold eat alabamian crab boil'),
 ((SELECT id FROM message_categories WHERE name = 'general'), 'https://tenor.com/view/himouto-umaru-chan-umaru-hamster-maracas-dance-gif-16363281'),
 ((SELECT id FROM message_categories WHERE name = 'general'), 'We should envite some slavs\nAND ask them if they like watamote'),
 ((SELECT id FROM message_categories WHERE name = 'general'), 'Watamote Is goated anime\nSadly to good to get season two\nTomoko should be in Mario kart world\nAlso in smash'),
-((SELECT id FROM message_categories WHERE name = 'general'), "It's like just imagine if people watch watamote season two they wold say something like ok this was too good gonna jump in a bridge because i Will never see something that good\nLogic"),
-((SELECT id FROM message_categories WHERE name = 'general'), "Not even trolling\nLike watamote manga Is goated\nThey can't make season two it's just imposible sadly"),
+((SELECT id FROM message_categories WHERE name = 'general'), 'It''s like just imagine if people watch watamote season two they wold say something like ok this was too good gonna jump in a bridge because i Will never see something that good\nLogic'),
+((SELECT id FROM message_categories WHERE name = 'general'), 'Not even trolling\nLike watamote manga Is goated\nThey can''t make season two it''s just imposible sadly'),
 ((SELECT id FROM message_categories WHERE name = 'general'), 'Pog fish'),
 ((SELECT id FROM message_categories WHERE name = 'general'), 'https://tenor.com/view/merry-merry-christmas-azumanga-daioh-chiyo-chichi-gif-2886624042494495607'),
-((SELECT id FROM message_categories WHERE name = 'general'), "What Is larping"),
+((SELECT id FROM message_categories WHERE name = 'general'), 'What Is larping'),
 ((SELECT id FROM message_categories WHERE name = 'general'), 'Shadow bonnie wold be in alabama for sure\nHe Is my persona'),
 ((SELECT id FROM message_categories WHERE name = 'general'), 'Orks are good too but murlocs are so special\nFunny fish'),
 ((SELECT id FROM message_categories WHERE name = 'general'), 'Better a schizo how Is Shadow bonnie'),
@@ -83,9 +81,8 @@ nAndrew wold eat alabamian crab boil'),
 ((SELECT id FROM message_categories WHERE name = 'hello_followup'), 'Mario or Sonic?'),
 ((SELECT id FROM message_categories WHERE name = 'hello_followup'), 'Thougs on Tomoko in Mario kart world?'),
 
-
 -- SSS Tier Opinions
-((SELECT id FROM message_categories WHERE name = 'sss_tier_opinions'), 'Seafood the best\nSSS top Tier food\nShadow bunny wold eat this for sure'), -- ./media/alabamaseafood.jpg
+((SELECT id FROM message_categories WHERE name = 'sss_tier_opinions'), 'Seafood the best\nSSS top Tier food\nShadow bunny wold eat this for sure'); -- ./media/alabamaseafood.jpg
 
 -- Attachments
 INSERT INTO message_attachments (message_id, file_path)
