cc8d139753dc463fbc153172806d1a3d3c8663a7
TheDragonary <112120245+TheDragonary@users.noreply.github.com>
2025-09-10 13:00:08 +0100

    Switched to Nvidia API and Kimi + Gemma, and updated the character prompt a little

--- aiSettings.js
+++ aiSettings.js
@@ -3,17 +3,18 @@
 // https://openrouter.ai/api/v1
 // http://localhost:5001/v1
 // https://generativelanguage.googleapis.com/v1beta/
-const baseURL = "https://generativelanguage.googleapis.com/v1beta/";
+// https://integrate.api.nvidia.com/v1
+const baseURL = "https://integrate.api.nvidia.com/v1";
 
 // If using a different online AI service, change this to your API key stored in ".env"
 // Examples: process.env.GEMINI_API_KEY, process.env.MISTRAL_API_KEY
 // If using local AI, change this to "0", although it doesn't really matter and you can leave it as-is
-const apiKey = process.env.GEMINI_API_KEY;
+const apiKey = process.env.NVIDIA_API_KEY;
 
 // If using koboldcpp, change ALL of these to "koboldcpp"
 // For vision to work locally, download the correct mmproj from https://huggingface.co/koboldcpp/mmproj/tree/main
 // Example: If you are using a model based on Llama3, download the one that says Llama3, then you would insert it into Loaded Files > Vision mmproj
-const gptModel = "gemini-2.5-flash";
-const gptimageModel = "gemini-2.5-flash";
+const gptModel = "moonshotai/kimi-k2-instruct";
+const gptimageModel = "google/gemma-3-27b-it";
 
 module.exports = { baseURL, apiKey, gptModel, gptimageModel };
