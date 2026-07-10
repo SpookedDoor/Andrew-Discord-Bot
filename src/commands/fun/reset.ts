7ccd17d4edc691ace491327a691a8576f0c42c47
SpookedDoor <Charatherealchara3@gmail.com>
2026-07-10 16:27:07 -0400

    Reset.js converted to Typescript

--- src/commands/fun/reset.ts
+++ src/commands/fun/reset.ts
@@ -1,8 +1,8 @@
-import { SlashCommandBuilder } from "discord.js";
+import { SlashCommandBuilder, ChatInputCommandInteraction } from "discord.js";
 
 export default {
     data: new SlashCommandBuilder().setName("reset").setDescription("Resets lil Androo!"),
-    async execute(interaction) {
+    async execute(interaction: ChatInputCommandInteraction) {
         await interaction.reply("<:tomoko_cup:1358095740299116614>");
     },
 };
